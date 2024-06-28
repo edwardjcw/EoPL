1a6b72e700e88e83eafd5a0b375c89efea6ae672
Edward Conrad <edwardjcw@gmail.com>
2024-06-28 05:29:49 -0500

    add unpack

--- EoPL/Parser.fs
+++ EoPL/Parser.fs
@@ -6,7 +6,7 @@ open LetLang
 let str = pstring
 let ws = spaces
 let strWs s = str s >>. ws
-let skipStrWs s = skipString s >>. ws
+//let skipStrWs s = skipString s >>. ws
 let pipe6 p1 p2 p3 p4 p5 p6 f = pipe4 p1 p2 p3 (tuple3 p4 p5 p6) (fun a b c (d, e, g) -> f a b c d e g)
 
 let pinteger : Parser<int, unit> = pint32 |>> int
@@ -16,9 +16,9 @@ let varExp : Parser<Exp, unit> = pvar |>> Exp.Var
 
 let pexp, pexpRef = createParserForwardedToRef<Exp, unit>()
 
-let letExp = skipStrWs "let" >>. many1 (skipStrWs "[" >>. pvar .>> ws .>> skipStrWs "=" .>>. pexp .>> ws .>> skipStrWs "]") .>> ws .>> skipStrWs "in" .>>. pexp |>> Exp.Let
-let letStarExp = skipStrWs "let*" >>. many1 (skipStrWs "[" >>. pvar .>> ws .>> skipStrWs "=" .>>. pexp .>> ws .>> skipStrWs "]") .>> ws .>> skipStrWs "in" .>>. pexp |>> Exp.LetStar
-let unpackExp = skipStrWs "unpack" >>. skipStrWs "[" >>. many1 pvar .>> ws .>> skipStrWs "=" .>>. pexp .>> ws .>> skipStrWs "]" .>> ws .>> skipStrWs "in" .>>. pexp |>> (fun ((vars, exp1), exp2) -> Exp.Unpack(vars, exp1, exp2))
+let letExp = skipString "let" >>. ws >>. many1 (skipString "[" >>. ws >>. pvar .>> ws .>> skipString "=" .>> ws .>>. pexp .>> ws .>> skipString "]" .>> ws) .>> skipString "in" .>> ws .>>. pexp |>> Exp.Let
+let letStarExp = skipString "let*" >>. ws >>. many1 (skipString "[" >>. ws >>. pvar .>> ws .>> skipString "=" .>> ws .>>. pexp .>> ws .>> skipString "]" .>> ws) .>> skipString "in" .>> ws .>>. pexp |>> Exp.LetStar
+let unpackExp = skipString "unpack" >>. ws >>. skipString "[" >>. ws >>. many1 (pvar .>> ws) .>> skipString "=" .>> ws .>>. pexp .>> ws .>> skipString "]" .>> ws .>> skipString "in" .>> ws .>>. pexp |>> (fun ((vars, exp1), exp2) -> Exp.Unpack(vars, exp1, exp2))
 let letTypeExp =
     choice [
         letStarExp
@@ -26,23 +26,23 @@ let letTypeExp =
         unpackExp
     ]
 
-let ifExp = skipStrWs "if" >>. pexp .>> ws .>> skipStrWs "then" .>>. pexp .>> ws .>> skipStrWs "else" .>>. pexp |>> (fun ((exp1, exp2), exp3) -> Exp.If(exp1, exp2, exp3))
-let condExp = skipStrWs "cond" >>. many1 (skipStrWs "(" >>. pexp .>> skipStrWs "==>" .>>. pexp .>> skipStrWs ")") .>> skipStrWs "end" |>> Exp.Cond
+let ifExp = skipString "if" >>. ws >>. pexp .>> ws .>> skipString "then" .>> ws .>>. pexp .>> ws .>> skipString "else" .>> ws .>>. pexp |>> (fun ((exp1, exp2), exp3) -> Exp.If(exp1, exp2, exp3))
+let condExp = skipString "cond" >>. ws >>. many1 (skipString "(" >>. ws >>. pexp .>> ws .>> skipString "==>" .>> ws .>>. pexp .>> skipString ")" .>> ws) .>> skipString "end" |>> Exp.Cond
 let conditionalExp = 
     choice [
         ifExp
         condExp
     ]
 
-let isZeroExp : Parser<Exp, unit> = skipStrWs "zero?(" >>. pexp .>> skipStrWs ")" |>> Exp.IsZero
-let minusExp = between (skipStrWs "minus(") (skipStrWs ")") pexp |>> Exp.Minus
-let addExp = skipStrWs "+(" >>. pexp .>> ws .>> skipStrWs "," .>>. pexp .>> skipStrWs ")" |>> (fun (exp1, exp2) -> Exp.Add(exp1, exp2))
-let diffExp = skipStrWs "-(" >>. pexp .>> ws .>> skipStrWs "," .>>. pexp .>> skipStrWs ")" |>> (fun (exp1, exp2) -> Exp.Diff(exp1, exp2))
-let multExp = skipStrWs "*(" >>. pexp .>> ws .>> skipStrWs "," .>>. pexp .>> skipStrWs ")" |>> (fun (exp1, exp2) -> Exp.Mult(exp1, exp2))
-let intDivExp = skipStrWs "/(" >>. pexp .>> ws .>> skipStrWs "," .>>. pexp .>> skipStrWs ")" |>> (fun (exp1, exp2) -> Exp.IntDiv(exp1, exp2))
-let isEqualExp = skipStrWs "=(" >>. pexp .>> ws .>> skipStrWs "," .>>. pexp .>> skipStrWs ")" |>> (fun (exp1, exp2) -> Exp.IsEqual(exp1, exp2))
-let isGreaterExp = skipStrWs ">(" >>. pexp .>> ws .>> skipStrWs "," .>>. pexp .>> skipStrWs ")" |>> (fun (exp1, exp2) -> Exp.IsGreater(exp1, exp2))
-let isLessExp = skipStrWs "<(" >>. pexp .>> ws .>> skipStrWs "," .>>. pexp .>> skipStrWs ")" |>> (fun (exp1, exp2) -> Exp.IsLess(exp1, exp2))
+let isZeroExp : Parser<Exp, unit> = skipString "zero?(" >>. ws >>. pexp .>> skipString ")" |>> Exp.IsZero
+let minusExp = between (skipString "minus(" .>> ws) (skipString ")") pexp |>> Exp.Minus
+let addExp = skipString "+(" >>. ws >>. pexp .>> ws .>> skipString "," .>> ws .>>. pexp .>> skipString ")" |>> Exp.Add
+let diffExp = skipString "-(" >>. ws >>. pexp .>> ws .>> skipString "," .>> ws .>>. pexp .>> skipString ")" |>> Exp.Diff
+let multExp = skipString "*(" >>. ws >>. pexp .>> ws .>> skipString "," .>> ws .>>. pexp .>> skipString ")" |>> Exp.Mult
+let intDivExp = skipString "/(" >>. ws >>. pexp .>> ws .>> skipString "," .>> ws .>>. pexp .>> skipString ")" |>> Exp.IntDiv
+let isEqualExp = skipString "=(" >>. ws >>. pexp .>> ws .>> skipString "," .>> ws .>>. pexp .>> skipString ")" |>> Exp.IsEqual
+let isGreaterExp = skipString ">(" >>. ws >>. pexp .>> ws .>> skipString "," .>> ws .>>. pexp .>> skipString ")" |>> Exp.IsGreater
+let isLessExp = skipString "<(" >>. ws >>. pexp .>> ws .>> skipString "," .>> ws .>>. pexp .>> skipString ")" |>> Exp.IsLess
 let mathOpExp =
     choice [
         isZeroExp
@@ -56,12 +56,12 @@ let mathOpExp =
         isLessExp
     ]
 
-let consExp = skipStrWs "cons(" >>. pexp .>> ws .>> skipStrWs "," .>>. pexp .>> ws .>> skipStrWs ")" |>> (fun (exp1, exp2) -> Exp.Cons(exp1, exp2))
-let carExp = skipStrWs "car(" >>. pexp .>> skipStrWs ")" |>> Exp.Car
-let cdrExp = skipStrWs "cdr(" >>. pexp .>> skipStrWs ")" |>> Exp.Cdr
-let isNullExp = skipStrWs "null?(" >>. pexp .>> skipStrWs ")" |>> Exp.IsNull
+let consExp = skipString "cons(" >>. ws >>. pexp .>> ws .>> skipString "," .>> ws .>>. pexp .>> ws .>> skipString ")" |>> Exp.Cons
+let carExp = skipString "car(" >>. ws >>. pexp .>> skipString ")" |>> Exp.Car
+let cdrExp = skipString "cdr(" >>. ws >>. pexp .>> skipString ")" |>> Exp.Cdr
+let isNullExp = skipString "null?(" >>. ws >>. pexp .>> skipString ")" |>> Exp.IsNull
 let emptyListExp : Parser<Exp, unit> = strWs "emptylist" |>> (fun _ -> Exp.EmptyList)
-let listExp = skipStrWs "list(" >>. sepBy pexp (skipStrWs ",") .>> skipStrWs ")" |>> Exp.List
+let listExp = skipString "list(" >>. ws >>. sepBy pexp (skipString "," .>> ws) .>> skipString ")" |>> Exp.List
 let listOpExp =
     choice [
         consExp
